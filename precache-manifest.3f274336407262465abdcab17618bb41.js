d0dbad8664c4a09a4838ea02d2fddbc9d0527683
Taggart Bowen-Gaddy <taggartbg+liftoffvr@gmail.com>
2019-11-03 17:29:24 -0500

    Updates

---
+++ precache-manifest.3f274336407262465abdcab17618bb41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7299975cfd56b4056caebce46a9d6e47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a6ceac58de40a72c47d",
+    "url": "/static/css/main.f2600eba.chunk.css"
+  },
+  {
+    "revision": "5d6c1378ac0f12917d0d",
+    "url": "/static/js/2.7abe3875.chunk.js"
+  },
+  {
+    "revision": "3a6ceac58de40a72c47d",
+    "url": "/static/js/main.4f9319c9.chunk.js"
+  },
+  {
+    "revision": "a734a42aee6266fdd920",
+    "url": "/static/js/runtime-main.35a1917d.js"
+  },
+  {
+    "revision": "0fa9ac4138f818860b3895f31e3b16c5",
+    "url": "/static/media/LiftOffVR_text.0fa9ac41.png"
+  },
+  {
+    "revision": "2c6a4eba97acff67fe0ee7f7b71afcc6",
+    "url": "/static/media/bg.2c6a4eba.jpg"
+  }
+]);
