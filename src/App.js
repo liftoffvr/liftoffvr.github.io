864354d5ba96c2a42e1c89af488a60ec4fca1b64
Taggart Bowen-Gaddy <taggart.bowen.gaddy@thesis.co>
2019-11-16 11:30:57 -0500

    Tracking added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import logo from './LiftOffVR_text.png';
 import './App.css';
 
-function App() {
+function App({ ad }) {
+  let formspreeId = ''
+
+  if (ad === 'fb1') {
+    formspreeId = 'mqkkgkkw'
+  } else if (ad === 'fb2') {
+    formspreeId = 'mzbbkbbq'
+  } else if (ad === 'g1') {
+    formspreeId = 'meqqpqqb'
+  } else if (ad === 'g2') {
+    formspreeId = 'xoqqpqqg'
+  }
+console.log("AD, ID", ad, formspreeId)
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
       <div className="form">
         <form
-          action="https://formspree.io/xvopnywg"
+          action={`https://formspree.io/${formspreeId}`}
           method="POST"
         >
           <label>
@@ -17,7 +29,7 @@ function App() {
             <input type="text" name="email" placeholder="email"/>
           </label>
           <br />
-          <button type="submit">Send</button>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+    <Router>
+        <Route path="/" exact component={App} />
+        <Route path="/g1" exact component={(props) => <App ad="g1" />} />
+        <Route path="/g2" exact component={(props) => <App ad="g2" />} />
+        <Route path="/fb1" exact component={(props) => <App ad="fb1" />} />
+        <Route path="/fb2" exact component={(props) => <App ad="fb2" />} />
+    </Router>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
